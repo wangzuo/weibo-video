f47322aae7c0de9d31a8e3352c41c8ac7110b8cc
Wang Zuo <wzuoadjusted@gmail.com>
2015-08-31 12:06:19 +0800

    with stream-progressbar

--- cli.js
+++ cli.js
@@ -1,23 +1,12 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var ProgressBar = require('progress');
+var progress = require('stream-progressbar');
 var PassThrough = require('stream').PassThrough;
 var video = require('./');
 
 var argv = process.argv;
 
-var progress = new PassThrough();
-
-progress.on('pipe', function(stream) {
-  stream.on('response', function(res) {
-    var total = parseInt(res.headers['content-length'], 10);
-    var bar = new ProgressBar(':bar', {total: total});
-
-    progress.on('data', function(chunk) {
-      bar.tick(chunk.length);
-    });
-  });
-});
-
-video(argv[2]).pipe(progress).pipe(fs.createWriteStream(argv[3]));
+video(argv[2])
+  .pipe(progress(':bar'))
+  .pipe(fs.createWriteStream(argv[3]));
